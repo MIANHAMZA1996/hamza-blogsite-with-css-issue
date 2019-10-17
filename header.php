8109deb0776a8a80b227daa97b88aeda8dac0f02
Muhammad Hamza <56311699+MIANHAMZA1996@users.noreply.github.com>
2019-10-17 16:10:38 +0500

    Initial commit

---
+++ header.php
@@ -0,0 +1,54 @@
+
+<link rel="stylesheet" type="text/css" href="index.css">
+
+<div class="main" style="" style="border-radius:60px">
+<!--header-->
+	<div class="header" >
+<!--image-->
+			<div class="logopic" >
+				<a href="check.html
+				">
+					<img src="logo.png" 
+					class="logopic1">
+				</a>
+			</div>			
+<!--Menu-->
+
+		<div class="menu" >
+			<a href="check.html
+			" class="home">Home</a>&nbsp
+			<a href="about US.html
+			" class="home" >About</a>&nbsp
+			<a href="contact us.html
+			" class="home" >Contact</a>&nbsp
+			<a href="post.html
+			" class="home" >Posts</a>&nbsp
+			<a href="category.html
+			" class="home" >Category&nbsp</a>
+			<a href="allusers.html
+			" class="home" >Users&nbsp</a>
+			<a href="quries.html
+			" class="home" >Quries</a>
+		</div>
+		
+		<div class="login">
+				<a href="logout.html
+				" class="login1"> Logout</a>			
+		</div>
+
+		<div class="login">
+				<a href="signup.html
+				" class="login1" >Sign up</a> 		
+		</div>
+		
+		<div class="login">
+				<a href="loginpage.html
+				" class="login1"> Login</a>
+		</div>
+
+	
+</div>
+</div>
+
+
+
